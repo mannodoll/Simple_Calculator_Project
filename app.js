9ce1713c871a346db08f273baceb16f3a26b70e2
mannodoll <mannodoll3@gmail/com>
2024-04-27 20:46:44 +0500

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+const calculator = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perform function",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+console.log(calculator);
+// conditional test
+if (calculator.operator === "Addition") {
+    console.log(calculator.firstNumber + calculator.secondNumber);
+}
+else if (calculator.operator === "Subtraction") {
+    console.log(calculator.firstNumber - calculator.secondNumber);
+}
+else if (calculator.operator === "Multiplication") {
+    console.log(calculator.firstNumber * calculator.secondNumber);
+}
+else if (calculator.operator === "Division") {
+    console.log(calculator.firstNumber / calculator.secondNumber);
+}
